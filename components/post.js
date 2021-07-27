a3cf005f1f517b732334927c157532b50d245203
Joseph Lynn <josephlynn94@gmail.com>
2021-07-27 10:14:40 -0400

    reworked post styles using tailwind

--- components/post.js
+++ components/post.js
@@ -1,26 +1,23 @@
 import Link from "next/link";
-import Date from "../components/date";
+import Date from "./date";
 import PostTags from "./tags";
+import Button from "./button";
 import postStyles from "../styles/post.module.css";
 
 const Post = ({ post }) => {
   return (
     <article className={postStyles.article} key={post.title}>
       <Date dateString={post.date} />
-      <Link href={`/posts/${post.id}`} className={postStyles.link}>
-        <a>
+      <Link href={`/posts/${post.id}`}>
+        <a className={postStyles.link}>
           <h3 className={postStyles.title}>{post.title}</h3>
         </a>
       </Link>
       <PostTags tags={post.tags} />
       <p className={postStyles.excerpt}>{post.excerpt}</p>
-      <div className={postStyles.details}>
-        <Link href={`/posts/${post.id}`} className={postStyles.link}>
-          <a>
-            <button className={postStyles.button}>Read More &rarr;</button>
-          </a>
-        </Link>
-      </div>
+      <Link href={`/posts/${post.id}`} className={postStyles.link}>
+        <a className={postStyles["read-more"]}>Read More &rarr;</a>
+      </Link>
     </article>
   );
 };
