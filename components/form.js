89a664e5c51544bbeb7954c5d5509a9de867dd10
Joseph Lynn <josephlynn94@gmail.com>
2021-07-30 10:22:16 -0400

    added form state, added netlify and next config files

--- components/form.js
+++ components/form.js
@@ -1,36 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import formStyles from "../styles/form.module.css";
+import buttonStyles from "../styles/button.module.css";
 
 const Form = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { value, name } = e.target;
+
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault;
+  };
+
+  console.log(formData);
   return (
     <form className={formStyles.container}>
       <div className={formStyles.control}>
         <label
-          htmlFor="full-name"
+          htmlFor="name"
           className="block text-sm font-medium text-gray-400"
         >
           Name
         </label>
         <input
           type="text"
-          name="full-name"
-          id="full-name"
+          name="name"
+          id="name"
           className={formStyles.input}
+          value={formData.name}
+          onChange={handleInputChange}
         />
       </div>
 
       <div className={formStyles.control}>
         <label
-          htmlFor="email-address"
+          htmlFor="email"
           className="block text-sm font-medium text-gray-400"
         >
           Email address
         </label>
         <input
           type="text"
-          name="email-address"
-          id="email-address"
+          name="email"
+          id="email"
           className={formStyles.input}
+          value={formData.email}
+          onChange={handleInputChange}
         />
       </div>
 
@@ -46,13 +71,16 @@ const Form = () => {
           name="message"
           id="message"
           className={`${formStyles.input} ${formStyles.textarea}`}
+          value={formData.message}
+          onChange={handleInputChange}
         />
       </div>
 
       <div className="mt-5">
         <button
           type="submit"
-          className="inline-flex justify-center py-2 px-3 border border-transparent font-medium text-secondary-100 bg-primary-100 hover:bg-primary-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-100"
+          className={`${buttonStyles.btn} ${buttonStyles.primary}`}
+          onClick={handleFormSubmit}
         >
           Send Message
         </button>
