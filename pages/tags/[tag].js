1c59521f21379506a96f94e82ab73b50547d1933
Joe Lynn <josephlynn94@gmail.com>
2021-08-02 22:40:05 -0400

    fixed tag pages not displaying tag name

--- pages/tags/[tag].js
+++ pages/tags/[tag].js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 import { getSortedPostsData } from "../../lib/posts";
 import Layout from "../../components/layout";
 import Post from "../../components/post";
@@ -50,6 +51,9 @@ export async function getStaticProps({ params }) {
 }
 
 const tag = ({ relatedPosts }) => {
+  const router = useRouter();
+  const { tag } = router.query;
+
   return (
     <Layout>
       <Head>
