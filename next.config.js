275891c26467982ab75cd49cba351130434c0dda
Joe Lynn <josephlynn94@gmail.com>
2022-06-11 17:43:20 -0400

    fix: adds security headers to website

--- next.config.js
+++ next.config.js
@@ -1,6 +1,28 @@
 const withImages = require("next-images");
 module.exports = withImages();
 
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';
+  child-src josephlynn.com;
+  style-src 'self' josephlynn.com;
+  font-src 'self';  
+`;
+
+const securityHeaders = [
+	{
+		key: "Content-Security-Policy",
+		value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+	},
+	{
+		key: "X-Content-Type-Options",
+		value: "nosniff",
+	},
+	{
+		key: "X-Frame-Options",
+		value: "SAMEORIGIN",
+	},
+];
 module.exports = {
 	reactStrictMode: true,
 	webpack5: true,
@@ -9,5 +31,16 @@ module.exports = {
 		config.resolve.fallback = { fs: false };
 
 		return config;
+	},
+	// You can choose which headers to add to the list
+	// after learning more below.
+	async headers() {
+		return [
+			{
+				// Apply these headers to all routes in your application.
+				source: "/:path*",
+				headers: securityHeaders,
+			},
+		];
 	},
 };
