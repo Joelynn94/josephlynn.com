c82a2a2e22abfea4d5919413c82d99ab65892e1c
Joe Lynn <josephlynn94@gmail.com>
2022-06-11 13:11:14 -0400

    feat: adds robots.txt and sitemap.xml

--- next.config.js
+++ next.config.js
@@ -2,5 +2,12 @@ const withImages = require("next-images");
 module.exports = withImages();
 
 module.exports = {
-    reactStrictMode: true,
+	reactStrictMode: true,
+	webpack5: true,
+	webpack: (config) => {
+		// Fixes npm packages that depend on `fs` module
+		config.resolve.fallback = { fs: false };
+
+		return config;
+	},
 };
