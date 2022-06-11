b688a08535ea516cbc7349c3946a513d6f78a6ee
Joe Lynn <josephlynn94@gmail.com>
2022-06-11 15:42:15 -0400

    fix: unique h1 for tag pages

--- pages/tags/[tag].js
+++ pages/tags/[tag].js
@@ -6,75 +6,75 @@ import Post from "../../components/post";
 import utilStyles from "../../styles/utils.module.css";
 
 export async function getStaticPaths() {
-  // tags to create pages from
-  const tags = [
-    "javascript",
-    "react",
-    "api",
-    "snippet",
-    "objects",
-    "arrays",
-    "todoapp",
-    "fetch",
-    "localstorage",
-    "classes",
-    "oop",
-    "inheritance",
-    "beginners",
-  ];
-  // create pages for each tag
-  const paths = tags.map((tag) => ({
-    params: {
-      tag,
-    },
-  }));
-  return {
-    paths,
-    fallback: false,
-  };
+	// tags to create pages from
+	const tags = [
+		"javascript",
+		"react",
+		"api",
+		"snippet",
+		"objects",
+		"arrays",
+		"todoapp",
+		"fetch",
+		"localstorage",
+		"classes",
+		"oop",
+		"inheritance",
+		"beginners",
+	];
+	// create pages for each tag
+	const paths = tags.map((tag) => ({
+		params: {
+			tag,
+		},
+	}));
+	return {
+		paths,
+		fallback: false,
+	};
 }
 
 export async function getStaticProps({ params }) {
-  // get all sorted posts
-  const postData = await getSortedPostsData();
+	// get all sorted posts
+	const postData = await getSortedPostsData();
 
-  // check how many posts have the tag name
-  const relatedPosts = postData.filter((data) =>
-    data.tags.includes(params.tag)
-  );
+	// check how many posts have the tag name
+	const relatedPosts = postData.filter((data) =>
+		data.tags.includes(params.tag)
+	);
 
-  return {
-    props: {
-      relatedPosts,
-    },
-  };
+	return {
+		props: {
+			relatedPosts,
+		},
+	};
 }
 
 const Tag = ({ relatedPosts }) => {
-  const router = useRouter();
-  const { tag } = router.query;
+	const router = useRouter();
+	const { tag } = router.query;
 
-  return (
-    <Layout>
-      <Head>
-        <title>Joseph Lynn | Tag: {tag}</title>
-      </Head>
-      <header className={`${utilStyles.section} py-8`}>
-        <h2>Posts tagged: {tag}</h2>
-        <h3>
-          {relatedPosts.length > 2
-            ? `${relatedPosts.length} posts found.`
-            : `${relatedPosts.length} post found.`}
-        </h3>
-      </header>
-      <section className={utilStyles.section}>
-        <h1 className={utilStyles.title}>Related Posts</h1>
-        {relatedPosts.map((post) => (
-          <Post key={post.id} post={post} />
-        ))}
-      </section>
-    </Layout>
-  );
+	return (
+		<Layout>
+			<Head>
+				<title>Joseph Lynn | Tag: {tag}</title>
+			</Head>
+			<header className={`${utilStyles.section} py-8`}>
+				<h2>Posts tagged: {tag}</h2>
+				<h3>
+					{relatedPosts.length > 2
+						? `${relatedPosts.length} posts found.`
+						: `${relatedPosts.length} post found.`}
+				</h3>
+			</header>
+			<section className={utilStyles.section}>
+				<h1 className={utilStyles.title}>Related Posts: {tag}</h1>
+				{relatedPosts.map((post) => (
+					<Post key={post.id} post={post} />
+				))}
+			</section>
+		</Layout>
+	);
 };
 
 export default Tag;
