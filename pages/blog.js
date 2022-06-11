fefbdc41fc295f2e7b09082a85180d3587cbe424
Joe Lynn <josephlynn94@gmail.com>
2022-06-11 16:42:58 -0400

    fix: custom meta titles for blog and projects pages

--- pages/blog.js
+++ pages/blog.js
@@ -5,27 +5,41 @@ import Layout from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
-  };
+	const allPostsData = getSortedPostsData();
+	return {
+		props: {
+			allPostsData,
+		},
+	};
 }
 
 const blog = ({ allPostsData }) => {
-  return (
-    <Layout>
-      <Head>
-        <title>Joseph Lynn | Blog</title>
-      </Head>
-      <section className={utilStyles.section}>
-        <h1 className={utilStyles.title}>Blog Posts</h1>
-        {allPostsData &&
-          allPostsData.map((post) => <Post key={post.id} post={post} />)}
-      </section>
-    </Layout>
-  );
+	return (
+		<Layout>
+			<Head>
+				<meta
+					name="description"
+					content="Personal blog for Joseph Lynn, a Frontend Web Developer based in Tampa, Florida. This is my own platform to write about topics I find important and where I can share my findings on the topics to help other developers."
+				/>
+				<meta
+					name="og:title"
+					content="Personal blog for Joseph Lynn, a Frontend Web Developer based in Tampa, Florida. This is my own platform to write about topics I find important and where I can share my findings on the topics to help other developers."
+				/>
+				<meta
+					name="twitter:description"
+					content="Personal blog for Joseph Lynn, a Frontend Web Developer based in Tampa, Florida. This is my own platform to write about topics I find important and where I can share my findings on the topics to help other developers."
+				/>
+				<title>Joseph Lynn | Blog</title>
+			</Head>
+			<section className={utilStyles.section}>
+				<h1 className={utilStyles.title}>Blog Posts</h1>
+				{allPostsData &&
+					allPostsData.map((post) => (
+						<Post key={post.id} post={post} />
+					))}
+			</section>
+		</Layout>
+	);
 };
 
 export default blog;
--- pages/projects.js
+++ pages/projects.js
@@ -6,22 +6,34 @@ import utilStyles from "../styles/utils.module.css";
 import projectStyles from "../styles/project.module.css";
 
 const projects = () => {
-  return (
-    <Layout>
-      <Head>
-        <title>Joseph Lynn | Projects</title>
-      </Head>
-      <section className={utilStyles.section}>
-        <h1 className={utilStyles.title}>Projects</h1>
-        <div className={projectStyles["grid-project"]}>
-          {projectData &&
-            projectData.map((project) => (
-              <Project key={project.id} project={project} />
-            ))}
-        </div>
-      </section>
-    </Layout>
-  );
+	return (
+		<Layout>
+			<Head>
+				<meta
+					name="description"
+					content="Personal projects for Joseph Lynn, a Frontend Web Developer based in Tampa, Florida. Here I share the projects I've worked on and proud to show off the work I've done."
+				/>
+				<meta
+					name="og:title"
+					content="Personal projects for Joseph Lynn, a Frontend Web Developer based in Tampa, Florida. Here I share the projects I've worked on and proud to show off the work I've done."
+				/>
+				<meta
+					name="twitter:description"
+					content="Personal projects for Joseph Lynn, a Frontend Web Developer based in Tampa, Florida. Here I share the projects I've worked on and proud to show off the work I've done."
+				/>
+				<title>Joseph Lynn | Projects</title>
+			</Head>
+			<section className={utilStyles.section}>
+				<h1 className={utilStyles.title}>Projects</h1>
+				<div className={projectStyles["grid-project"]}>
+					{projectData &&
+						projectData.map((project) => (
+							<Project key={project.id} project={project} />
+						))}
+				</div>
+			</section>
+		</Layout>
+	);
 };
 
 export default projects;
